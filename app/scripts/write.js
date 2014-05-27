45b878c774a4c53ede2b67a636c527b52bf8e48e
Daniel Serrano <dserrano3@dserrano3-Inspiron-N5010.(none)>
2014-05-26 21:08:13 -0500

    Nueva funcion para leer y escribir valores en la base de datos

--- app/scripts/write.js
+++ app/scripts/write.js
@@ -8,13 +8,34 @@ define(function (require) {
     var $ = require('jquery'), a = {},
 
         write = function (valueToWrite, fecha) {
-            if (!a[fecha]) {a[fecha] = []; }
-            a[fecha].push(valueToWrite);
+           $.ajax( { url: "https://api.mongolab.com/api/1/databases/dserrano3/collections/my-coll?apiKey=5pIQfbFNaIani-w-ZfLb97QOR6xf3KRw",
+		  data: JSON.stringify( { fecha : valueToWrite } ),
+		  type: "POST",
+		  contentType: "application/json" } );
         },
         
+        //funcion auxiliar para encontrar valor en DB, se neesitan 2 por lo que es asincrono.
+        function getval( callback ){
+            jQuery.getJSON('https://api.mongolab.com/api/1/databases/happiness/collections/my-coll?apiKey=5pIQfbFNaIani-w-ZfLb97QOR6xf3KRw',            function(data) {
+            // We can't use .return because return is a JavaScript keyword.
+            callback(data[0]);
+        });
+        
+        //Funcion para ser llamada desde otro js, retorna el valor de la fecha que se le pasa.
         find = function(fecha) {
-            return a[fecha];
+            var retorno;
+            $(document).ready(function() {
+                getval( function ( value ) { 
+                    retorno = value;
+                } );
+            });
+            // Este valor es un mapa se puede hacer retorno['x'] para probar.
+            return retorno[fecha];
         };
+        
+        
+}
+    
 
     // The public API
     return {
